6cc506b89f5e4c157075d6a193bd67fb58843305
Angel González <agonzalez@nscontrol.es>
2019-12-03 21:07:53 +0100

    Initial Commit

---
+++ MeplesShuffler/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MeplesShuffler
+//
+//  Created by Angel Esteban González Fuentes on 03/12/2019.
+//  Copyright © 2019 Angel Gonzalez Fuentes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
